e3ae0d3c2144b1ebcf44f12513bd1c28001787f6
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2024-03-17 17:18:46 +0100

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,47 +1,75 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ForgetPass from "./ForgetPass";
+import { state, showGetPass } from "../redux/redux";
 export default function Login() {
+    const navigate = useNavigate();
+    const passStatus = state().passStatus;
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (userName && password) {
+            console.log(userName);
+            console.log(password);
+            navigate("/schema");
+        } else {
+            alert("Please complete inputs");
+        }
+    };
     return (
         <div className="w-full flex items-center">
-            <div className="w-full h-full flex flex-col items-center ">
-                <form className="text-center  ">
-                    <div className="w-full text-center flex justify-center">
-                        <h3 className="w-[80px] h-[36px] font-[700] text-[20px] leading-[36px] text-black text-center">
-                            Logga in
-                        </h3>
-                    </div>
-                    <label className="flex items-end w-[371px]  h-[70px] border-b-[1px] border-b-slate-600 mb-[23px] pb-1 cursor-pointer">
-                        <i className="fa-regular fa-user w-[63px]  text-[23px]  p-1 text-center text-slate-900  "></i>
-                        <input
-                            type="text"
-                            placeholder="Användernamn"
-                            className="w-[155px] h-[29px] font-[400] text-[16px]  outline-none placeholder-black "
-                        />
-                    </label>
-                    <label className="flex items-end  w-[371px] h-[70x] border-b-[1px] border-b-slate-600 mb-[23px] pb-1 cursor-pointer">
-                        <i className="fa-solid fa-lock w-[63px]  text-[23px] text-center p-1 text-slate-700 "></i>
-                        <input
-                            type="text"
-                            placeholder="Lösenord"
-                            className="w-[155px] h-[29px] font-[400] text-[16px]  outline-none placeholder-black"
-                        />
-                    </label>
+            {passStatus ? (
+                <ForgetPass />
+            ) : (
+                <div className="w-full h-full flex flex-col items-center ">
+                    <form onSubmit={handleSubmit} className="text-center  ">
+                        <div className="w-full text-center flex justify-center">
+                            <h3 className="w-[80px] h-[36px] font-[700] font-din_medium text-[20px] leading-[36px] text-black text-center">
+                                Logga in
+                            </h3>
+                        </div>
+                        <label className="flex items-end w-[371px]  h-[70px]  border-b-[1px] border-b-slate-600  pb-1 cursor-pointer mb-5 ">
+                            <i className="fa-regular fa-user w-[63px]  text-[23px]  p-1 text-center text-slate-900  "></i>
+                            <input
+                                type="text"
+                                placeholder="Användernamn"
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
+                                className="w-[155px] h-[29px] font-[400] font-din text-[20px]  outline-none placeholder-black "
+                            />
+                        </label>
+                        <label className="flex items-end w-[371px]  h-[70px]  border-b-[1px] border-b-slate-600  pb-1 cursor-pointer ">
+                            <i className="fa-solid fa-lock w-[63px]  text-[23px] text-center p-1 text-slate-700 "></i>
+                            <input
+                                type="password"
+                                placeholder="Lösenord"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                autoComplete=""
+                                className="w-[155px] h-[29px] font-[400] font-din text-[20px]  outline-none placeholder-black"
+                            />
+                        </label>
+                        <button
+                            type="submit"
+                            className="w-[229px] m-[20px] bg-[#4E69F6] rounded-[12px]  text-center cursor-pointer "
+                        >
+                            <span className="w-[80px] font-[700] font-din_medium text-[16px] leading-[36px]   text-white">
+                                Logga in
+                            </span>
+                        </button>
+                    </form>
                     <button
-                        type="submit"
-                        className="w-[229px] bg-[#4E69F6] rounded-[12px]  text-center cursor-pointer "
+                        type="button"
+                        onClick={showGetPass}
+                        className="w-[280px]   flex justify-end items-end text-start   "
                     >
-                        <span className="w-[80px] h-[30px] font-[700] text-[16px] leading-[36px]   text-white">
-                            Logga in
+                        <span className="  text-[16px]  border-b-[1px] border-b-slate-400 cursor-pointer text-end">
+                            Glömt lösenord?
                         </span>
                     </button>
-                </form>
-                <button
-                    type="button"
-                    className="w-[280px] h-[34px]  flex justify-end items-end text-end  pt-10"
-                >
-                    <span className="  text-[16px]  border-b-[1px] border-b-slate-400 cursor-pointer text-end">
-                        Glömt lösenord?
-                    </span>
-                </button>
-            </div>
+                </div>
+            )}
         </div>
     );
 }
