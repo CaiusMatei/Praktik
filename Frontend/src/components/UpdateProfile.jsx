9b16a2f5cb989a4c2e16d2a6aced9d42ce4a017c
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2024-03-17 22:57:15 +0100

    Update UpdateProfile.jsx

--- Frontend/src/components/UpdateProfile.jsx
+++ Frontend/src/components/UpdateProfile.jsx
@@ -1,6 +1,11 @@
 
 export default function UpdateProfile() {
   return (
-    <div>UpdateProfile</div>
+    <div>
+        <div className=" border w-[709px] h-[673px] ml-[59px] rounded-[12px] my-[56px] bg-[#f1f1f1] ">
+
+        </div>
+        <div></div>
+    </div>
   )
 }
