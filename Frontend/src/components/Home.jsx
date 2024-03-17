9ebae019139d78f802193b1120e6536474f76ee6
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2024-03-17 17:18:30 +0100

    Update Home.jsx

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -15,12 +15,12 @@ export default function Home() {
                     className="rounded-[12px]  h-full  flex justify-center "
                     style={linearGradientStyle}
                 >
-                    <p className="w-[610px] h-[139px] m-[24px] mt-[471px] font-[700] text-[16px] leading-[27px] text-white ">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
+                    <p className="w-[610px] h-[139px] m-[24px] mt-[471px] font-[700] font-din_medium text-[16px] leading-[27px] text-white ">
+                        Lorem ipsum dolor sit amet, @ consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat. Duis aute irure dolor in
                         reprehenderit in voluptate velit esse cillum dolore eu
                         fugiat nulla pariatur. Excepteur sint occaecat cupidatat
                         non proident, sunt in culpa qui officia deserunt mollit
