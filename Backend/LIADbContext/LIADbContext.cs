2471bbd0cea72d56d3f4373a1bce46b2a44b584a
Toxicwhisper <morgan.lindzen@gmail.com>
2024-03-20 12:34:03 +0100

    added dbcontext

---
+++ Backend/LIADbContext/LIADbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.LIADbContext
+{
+    public class LIADbContext : DbContext
+    {
+        public LIADbContext(DbContextOptions<LIADbContext> options) : base(options)
+        {
+        }
+    }
+}
