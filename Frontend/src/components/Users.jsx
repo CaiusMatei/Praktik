ed9968eaee1c7de350baace63299e2d70dd04452
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2024-03-23 23:18:20 +0100

    Update Users.jsx

--- Frontend/src/components/Users.jsx
+++ Frontend/src/components/Users.jsx
@@ -1,10 +1,12 @@
 export default function Users() {
     return (
         <div>
-            <div className="relative overflow-x-auto mx-[85px]">
-                <h1 className="w-[224px] my-[38px] ">Användar lista</h1>
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-900 uppercase dark:text-gray-400">
+            <div className="relative overflow-x-auto mx-[85px] ">
+                <h1 className="w-[224px] my-[38px] font-din_medium text-[30px] ">
+                    Användar lista
+                </h1>
+                <table className="w-full text-sm text-left rtl:text-right text-black dark:text-black ">
+                    <thead className="text-[20px] font-din   dark:text-black text-center  ">
                         <tr>
                             <th scope="col" className="px-6 py-3">
                                 Namn
@@ -20,108 +22,150 @@ export default function Users() {
                             </th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th
+                    <tbody className="text-center   ">
+                        <tr className="bg-[#F2F2F2]   dark:text-black text-[20px] hover:bg-[#EE8091]  ">
+                            <td
                                 scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                className="px-6 py-4 font-medium text-black border border-[#aea9a9]    "
                             >
                                 Noa
-                            </th>
-                            <td className="px-6 py-4">Eklund</td>
-                            <td className="px-6 py-4">Noaeklund@gmail.com</td>
-                            <td className="px-6 py-4">NoaEk</td>
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Eklund
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Noaeklund@gmail.com
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                NoaEk
+                            </td>
                         </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th
+                        <tr className="bg-[#F2F2F2]  dark:text-black text-[20px] ">
+                            <td
                                 scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                className="px-6 py-4 font-medium text-black border border-[#aea9a9]  "
                             >
                                 Alex
-                            </th>
-                            <td className="px-6 py-4">Ibrahimovic</td>
-                            <td className="px-6 py-4">
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Ibrahimovic
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
                                 AlexIbrahimovic@gmail.com
                             </td>
-                            <td className="px-6 py-4">Alex.ic</td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Alex.ic
+                            </td>
                         </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th
+                        <tr className="bg-[#F2F2F2]  dark:text-black text-[20px] ">
+                            <td
                                 scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                className="px-6 py-4 font-medium text-black border border-[#aea9a9]  "
                             >
                                 Gabriela
-                            </th>
-                            <td className="px-6 py-4">Nordström</td>
-                            <td className="px-6 py-4">
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Nordström
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
                                 Gabrielanordstrm@gmail.com
                             </td>
-                            <td className="px-6 py-4">Gabstr</td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Gabstr
+                            </td>
                         </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th
+                        <tr className="bg-[#F2F2F2]  dark:text-black text-[20px] ">
+                            <td
                                 scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                className="px-6 py-4 font-medium text-black border border-[#aea9a9]  "
                             >
                                 Rafael
-                            </th>
-                            <td className="px-6 py-4">Charles</td>
-                            <td className="px-6 py-4">
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Charles
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
                                 Rafaelcharles14@gmail.com
                             </td>
-                            <td className="px-6 py-4">Raf123</td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Raf123
+                            </td>
                         </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th
+                        <tr className="bg-[#F2F2F2]  dark:text-black text-[20px] ">
+                            <td
                                 scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                className="px-6 py-4 font-medium text-black border border-[#aea9a9]  "
                             >
                                 Lucas
-                            </th>
-                            <td className="px-6 py-4">Penchas</td>
-                            <td className="px-6 py-4">
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Penchas
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
                                 Lucaspenchas@gmail.com
                             </td>
-                            <td className="px-6 py-4">Lucaspenchas</td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Lucaspenchas
+                            </td>
                         </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th
+                        <tr className="bg-[#F2F2F2]  dark:text-black text-[20px] ">
+                            <td
                                 scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                className="px-6 py-4 font-medium text-black border border-[#aea9a9]  "
                             >
                                 Amadeus
-                            </th>
-                            <td className="px-6 py-4">Rodriguez</td>
-                            <td className="px-6 py-4">Amadeus@gmail.com</td>
-                            <td className="px-6 py-4">Amrodiguez</td>
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Rodriguez
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Amadeus@gmail.com
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Amrodiguez
+                            </td>
                         </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th
+                        <tr className="bg-[#F2F2F2]  dark:text-black text-[20px] ">
+                            <td
                                 scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                className="px-6 py-4 font-medium text-black border border-[#aea9a9]  "
                             >
                                 Isabela
-                            </th>
-                            <td className="px-6 py-4">Aleesandro</td>
-                            <td className="px-6 py-4">Aleesandro@gmail.com</td>
-                            <td className="px-6 py-4">Isabela717</td>
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Aleesandro
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Aleesandro@gmail.com
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Isabela717
+                            </td>
                         </tr>
-                        <tr className="bg-white dark:bg-gray-800">
-                            <th
+                        <tr className="bg-[#F2F2F2]  dark:text-black text-[20px] ">
+                            <td
                                 scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                className="px-6 py-4 font-medium text-black border border-[#aea9a9]  "
                             >
                                 Amanda
-                            </th>
-                            <td className="px-6 py-4">Bylund</td>
-                            <td className="px-6 py-4">
-                            Amanda.by@gmail.com
                             </td>
-                            <td className="px-6 py-4">AmandaB</td>
+                            <td className="px-6 py-4 border border-[#aea9a9]  ">
+                                Bylund
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                Amanda.by@gmail.com
+                            </td>
+                            <td className="px-6 py-4 border border-[#aea9a9] ">
+                                AmandaB
+                            </td>
                         </tr>
                     </tbody>
                 </table>
-                <button>Ta bort konto</button>
+                <div className="  text-end my-10">
+                    <button className="w-[317px ] px-[77px] h-[66px] bg-[#EE8091] rounded-[7px] font-din_medium text-[20px]">
+                        Ta bort konto
+                    </button>
+                </div>
             </div>
         </div>
     );
