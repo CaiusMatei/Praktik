cbdfa77af556c483fcf81372d12baf01922ffb7e
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2024-03-23 21:18:45 +0100

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,17 +5,21 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import ForgetPass from "./components/ForgetPass";
 import UpdateProfile from "./components/UpdateProfile";
+import AddUser from "./components/AddUser";
+import Users from "./components/Users";
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Header />
                 <Routes>
-                <Route path="/" element = {<Home/>} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/schema" element={<Schema />} />
-                    <Route path="/forgerPass" element = {<ForgetPass/>} />
-                    <Route path="/updateProfile" element = {<UpdateProfile/>} />
+                    <Route path="/forgerPass" element={<ForgetPass />} />
+                    <Route path="/updateProfile" element={<UpdateProfile />} />
+                    <Route path="/addUser" element={<AddUser />} />
+                    <Route path="/users" element={<Users />} />
                 </Routes>
             </BrowserRouter>
         </>
