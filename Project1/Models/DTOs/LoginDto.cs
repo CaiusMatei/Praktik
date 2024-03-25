4cd0dc2be5535a01ebcdb6d72985afc2b14e969d
Toxicwhisper <morgan.lindzen@gmail.com>
2024-03-25 12:55:19 +0100

    restructured program to include react client for login and log out

---
+++ Project1/Models/DTOs/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project1.Models.DTOs
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "Please enter an email address.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password.")]
+        public string? Password { get; set; }
+    }
+}
