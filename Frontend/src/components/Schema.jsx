755d061ce50e78bcc919f21f1a41a3bfac326b51
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2024-03-25 12:23:41 +0100

    Update Schema.jsx

--- Frontend/src/components/Schema.jsx
+++ Frontend/src/components/Schema.jsx
@@ -1,7 +1,27 @@
 export default function Schema() {
     return (
-        <div className=" flex top-0 right-0">
-            <h1>Schema </h1>
+        <div className=" flex flex-col">
+            <div className="ml-[130px] mt-[8px]">
+                <div className="flex ">
+                    <div className=" w-[35%]">
+                        <h1 className="font-din_medium text-[20px] mb-[16px] font-[700]">
+                            Schema
+                        </h1>
+                        <h2 className="font-din_medium text-[20px] mb-[16px] font-[700]">
+                            Schema för UXK22
+                        </h2>
+                        <p className="text-[14px] font-din text-[#00000099]">
+                            23 timme förvecka 35{" "}
+                        </p>
+                    </div>
+                    <div className="w-[341px] h-[56px]  rounded-[12px] flex justify-around items-center shadow-md shadow-gray-400 *:font-din_medium *:text-[20px]  *:font-[700]  *:rounded-[12px] *:w-full *:h-full">
+                        <button>dag</button>
+                        <button>vecka</button>
+                        <button>månad</button>
+                    </div>
+                </div>
+            </div>
+            <div></div>
         </div>
     );
 }
