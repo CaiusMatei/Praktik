ec72caa28b46047328f8c80a890fa56267285d47
Saga Andersson <sagander@hotmail.com>
2024-03-27 11:14:49 +0100

    Added Role Class

---
+++ Backend/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+        public string RoleType { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User Users { get; set; }
+
+    }
+}
