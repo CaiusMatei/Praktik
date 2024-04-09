720e9e19dbc494c16112b5aa447820fae3c1d20e
BaqerAlabedi <112864227+BaqerAlabedi@users.noreply.github.com>
2024-04-09 13:54:01 +0200

    shadcn

--- Backend2/ClientApp/src/components/NavMenu.js
+++ Backend2/ClientApp/src/components/NavMenu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+// eslint-disable-next-line
 import nightwind from "nightwind/helper"
 
 export class NavMenu extends Component {
